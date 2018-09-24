6dce8412ec79590eecb5698b40ebc50da4c06338
George Stelle <stelleg@lanl.gov>
2018-09-24 14:39:24 -0600

    Added concreate safe concurrency and noninterference example, lh failing where it shouldn't be though

--- examples.hs
+++ examples.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 import Data.Set
 import Foreign.Ptr
@@ -9,25 +10,32 @@ import Foreign.Marshal.Array
 import Control.Monad.IO.Class
 import Control.Concurrent.Async
 
-{-@ type Region = {v:(Nat,Nat) | fst v <= snd v} @-}
+{-@ type RegionT = {v:(Nat,Nat) | fst v <= snd v} @-}
 type Region = (Int, Int)
 
-{-@ type SubRegion p = {v:Region | snd v < plen p} @-}
-type SubRegion p = Region
+{-@ type SubRegion P = {v:RegionT | snd v < plen P} @-}
 
-{-@ measure interfere :: (Int,Int) -> (Int,Int) -> Bool @-}
-interfere (l, h) (l', h') = l >= l' && l <= h' || h >= l' && h <= h'
+{-@ measure interfere :: Region -> Region -> Bool @-}
+interfere :: Region -> Region -> Bool
+interfere (l, h) (l', h') = l >= l' && l <= h' || h >= l' && h <= h' || l <= l' && h >= h'
 
 {-@ type OkPtr a = {v:Ptr a | 0 < plen v} @-}
 {-@ type In r = {x:Nat | x >= fst r  && x <= snd r} @-}
 {-@ measure ioRange :: IO a -> Ptr b -> Int -> Int -> Bool @-}
 {-@ assume mallocArray :: Storable a => n:Nat -> IO (PtrN a n) @-}
 {-@ assume withArray :: Storable a => x:[a] -> (PtrN a (len x) -> IO b) -> IO b @-}
+{-@ assume newArray :: Storable a => x:[a] -> IO (PtrN a (len x)) @-}
 {-@ assume peekElemOff :: Storable a => p:Ptr a -> x:{n:Nat | n < plen p} -> IO a @-}
 {-@ assume pokeElemOff :: Storable a => p:Ptr a -> x:{n:Nat | n < plen p} -> a -> IO () @-}
 {-@ assume advancePtr :: Storable a => o:Ptr a -> x:Nat -> {v : PtrN a m | plen v == plen o - x} @-}
 
-newtype PIO a = MkPIO { getIO :: IO a } deriving Functor
+{-@ measure safe :: Perm -> Perm -> Bool @-}
+safe :: Perm -> Perm -> Bool
+safe R R = True
+safe _ _ = False
+
+data Perm = R | W
+newtype PIO p i a = MkPIO { getIO :: IO a } deriving Functor
 
 instance Applicative PIO where
   MkPIO x <*> MkPIO y = MkPIO (x <*> y)
@@ -40,6 +48,10 @@ instance Monad PIO where
 instance MonadIO PIO where
    liftIO = MkPIO   
 
+{-@ exampleNonInterfere :: {p : (RegionT, RegionT) | not (interfere (fst p) (snd p))} @-}
+exampleNonInterfere :: (Region, Region)
+exampleNonInterfere = ((3, 4), (5, 6))
+
 {-@ withConstArray :: Storable a => a -> n:Nat -> (PtrN a n -> IO b) -> IO b @-}
 withConstArray x n = withArray (replicate n x) 
 
@@ -51,33 +63,31 @@ regionPeekElemOff p r x = peekElemOff p x
 regionPokeElemOff :: Storable a => Ptr a -> Region -> Int -> a -> IO ()
 regionPokeElemOff p r x a = pokeElemOff p x a
 
-{-@ safeConc :: p:Ptr a -> v:SubRegion p -> v':{v':SubRegion p | not (interfere v v')} ->
-                   (Ptr a -> SubRegion p -> IO b) -> 
-                   (Ptr a -> SubRegion p -> IO c) -> 
-                   IO (b,c) @-}
-safeConc :: Ptr a -> Region -> Region -> 
-            (Ptr a -> Region -> IO b) ->
-            (Ptr a -> Region -> IO c) -> 
-            IO (b, c)
-safeConc p r r' f g = concurrently (f p r) (g p r')
+{-@ safeConc :: p:Ptr a -> x:SubRegion p -> y:{y':SubRegion p | not (interfere x y')} ->
+                   IO b -> IO c -> IO (b,c) @-}
+safeConc :: Ptr a -> Region -> Region -> IO b -> IO c -> IO (b, c)
+safeConc p r r' f g = concurrently f g
 
---{-@ regionMalloc :: Storable a => r:SubRegion p -> x:In r -> RegionIO p r a @-}
-regionMalloc = undefined 
-
-{-{-@ regionPeeks :: Storable a => p:Ptr a -> r:SubRegion p -> IO [a] @-}
-regionPeeks :: Storable a => Ptr a -> Region -> IO [a]
-regionPeeks p (s,e) = mapM (regionPeekElemOff p (s,e)) [s..e]
--}
-
-{-@ regionPeekElemOff :: Storable a => p:Ptr a -> r:SubRegion p -> x:In r -> IO a @-}
-f :: Ptr Char -> Region -> IO Char 
-f p r = regionPeekElemOff p r 1
-
-g :: Ptr Char -> Region -> IO Char
-g p r = regionPeekElemOff p r 2
+{-@ regionPeek :: Storable a => p:Ptr a -> r:SubRegion p -> IO [a] @-}
+regionPeek :: Storable a => Ptr a -> Region -> IO [a]
+regionPeek p (s,e) = if s < e then do
+    v <- peekElemOff p s 
+    vs <- regionPeek p (succ s, e) 
+    return (v:vs)
+  else if s == e then do
+    v <- peekElemOff p s
+    return [v]
+  else 
+    return []
 
 main :: IO ()
-main = withArray "hello world" $ \arr -> do
+main = do
+  arr <- newArray "hello world" 
   c <- peekElemOff arr 3 
-  (one, two) <- safeConc arr (0, 1) (2, 3) f g
-  print c
+  let r1 = (0, 5)
+  let r2 = (6, 10)  
+  (one, two) <- safeConc arr r1 r2
+                  (regionPeekElemOff arr r1 0) 
+                  (regionPeekElemOff arr r2 7)
+  print (one, two)
+
